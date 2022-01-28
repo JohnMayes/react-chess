5f4b07e001178f9da6b625efc331277cbbeb1220
JohnMayes <36653554+JohnMayes@users.noreply.github.com>
2022-01-28 12:19:45 -0600

    FEN init

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,46 @@ function App() {
     // console.log(pieceArr);
   };
 
+  const numToEmptyStrings = (num: number) => {
+    let arr = [];
+    for (let i = num; i > 0; i--) {
+      arr.push('');
+    }
+    return arr;
+  };
+
+  type SpreadType = string[] | string[][];
+
+  function setPosition(fen: string) {
+    if (fen) {
+      // Parses FEN
+      const parts = fen.replace(/^\s*/, '').replace(/\s*$/, '').split(/\/|\s/);
+      // Slices pieces position of FEN by row
+      const rows = parts.slice(0, 8);
+      // Splits each row into it's piece notation
+      let arrFEN = [];
+      for (let e of rows) {
+        const spread: any = e.split('');
+        // If string is a numeral, converts to a number
+        for (let i = 0; i < spread.length; i++) {
+          let char = spread[i];
+          let num = Number(char);
+          if (Number.isNaN(num) === false) {
+            spread[i] = numToEmptyStrings(num);
+          }
+        }
+        e = spread.flat();
+        arrFEN.push(e);
+      }
+      const flatFEN = arrFEN.flat();
+      console.log(flatFEN);
+    }
+  }
+
+  // console.log(numToEmptyStrings(8));
+  console.log(board);
+  setPosition('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+
   const handleClick = (e: SyntheticEvent, cord: string) => {
     setSquare(cord);
     let move = chess.current.move({
